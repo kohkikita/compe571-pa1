005281d74aa7d4592f035dba76679fadc205a598
Ethan Quach <ethanquachwork@gmail.com>
2025-09-28 21:17:51 -0700

    test

--- src/config.h
+++ src/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-static const long long N = 100000000; // 1000000000, 10000000000 // Adjust N as needed for performance testing
+static const long long N = 100; // 1000000000, 10000000000 // Adjust N as needed for performance testing
 static const int NUM_THREADS = 2; // 4, 8
 static const int NUM_TASKS = 2; // 4, 8 
 
